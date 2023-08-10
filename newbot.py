102463ebd337f296389cd9d217f6eb3cc8e26823
pwill0217 <73377167+pwill0217@users.noreply.github.com>
2023-08-10 13:35:08 -0500

    Update newbot.py

--- newbot.py
+++ newbot.py
@@ -2,17 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
-username = ('gooodwillll')
-password = ('lolsir217')
+username = ('YourInstagramUsername')
+password = ('YourInstagramPassword')
 
-chromedriver_path = '/usr/local/bin/chromedriver 2' # Change this to your own chromedriver path!
+chromedriver_path = '/Your/chromedriver/path' # Change this to your own chromedriver path! Also please check your webdrive version
 webdriver = webdriver.Chrome()
 sleep(2)
 webdriver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
 sleep(3)
 username = webdriver.find_element_by_name('username')
-username.send_keys('gooodwillll')
+username.send_keys('YourInstagramUsername')
 password = webdriver.find_element_by_name('password')
-password.send_keys('lolsir217')
+password.send_keys('YourInstagramPassword')
 submit = webdriver.find_element_by_tag_name('form')
 submit.submit()
